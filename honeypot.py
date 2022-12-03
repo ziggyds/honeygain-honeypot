902b99a25d4081f13227eb33d472d370e748fdc5
zidesm <76918762+zidesm@users.noreply.github.com>
2022-12-03 17:02:09 +0100

    init new honeypot bot

---
+++ honeypot.py
@@ -0,0 +1,33 @@
+from pyHoneygain import HoneyGain
+from discord_webhook import DiscordWebhook
+from datetime import date
+import os
+
+# Get jwt and webhook url from env var
+JWT = os.getenv('JWT_TOKEN')
+DISCORD = os.getenv('DISCORD_WEBHOOK')
+
+user = HoneyGain()
+user.set_jwt_token(JWT)
+
+user_balance = "\nUser HG balance {}, JT balance {}".format(user.wallet_stats()["data"]["{}".format(date.today())]["hg_credits"], user.wallet_stats()["data"]["{}".format(date.today())]["jt_credits"])
+honeypot_status = user.get_honeypot_status()
+message = "error"
+if str(honeypot_status["winning_credits"]) != "None":
+    message = "{} already claimed the honeypot today.{}".format(user.me()['email'], user_balance)
+    webhook = DiscordWebhook(url=DISCORD, content='{}'.format(message))
+else:
+    if honeypot_status["progress_bytes"] == honeypot_status["max_bytes"]:
+        result = user.open_honeypot()
+        if result["success"]:
+            message = "Opened honeypot for {}, resuting in {} credits.{}".format(user.me()['email'], result["credits"], user_balance)
+            webhook = DiscordWebhook(url=DISCORD, content='{}'.format(message))
+        else:
+            message = "Couldn\'t open honeypot for {}, unknown error.{}".format(user.me()['email'], user_balance)
+            webhook = DiscordWebhook(url=DISCORD, content='{}'.format(message))
+    else:
+        message = "Couldn\'t open honeypot for {}, did not gather 15MB.{}\nProgress in bytes: {}/15000000".format(user.me()['email'], user_balance, user.get_honeypot_status()["progress_bytes"])
+        webhook = DiscordWebhook(url=DISCORD, content='{}'.format(message))
+
+print(message)
+response = webhook.execute()
