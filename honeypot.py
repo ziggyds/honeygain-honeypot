cc27e6c500fac2b31b98349a2efd164f2ff98907
zidesm <76918762+zidesm@users.noreply.github.com>
2022-12-04 13:21:12 +0100

    typo and clean up

--- honeypot.py
+++ honeypot.py
@@ -14,20 +14,17 @@ user_balance = "\nUser HG balance {}, JT balance {}".format(user.wallet_stats()[
 honeypot_status = user.get_honeypot_status()
 message = "error"
 if str(honeypot_status["winning_credits"]) != "None":
-    message = "{} already claimed the honeypot today.{}".format(user.me()['email'], user_balance)
-    webhook = DiscordWebhook(url=DISCORD, content='{}'.format(message))
+    message = "{} already claimed the honeypot today.{}".format(user.me()['email'],  user_balance)
 else:
     if honeypot_status["progress_bytes"] == honeypot_status["max_bytes"]:
         result = user.open_honeypot()
         if result["success"]:
-            message = "Opened honeypot for {}, resuting in {} credits.{}".format(user.me()['email'], result["credits"], user_balance)
-            webhook = DiscordWebhook(url=DISCORD, content='{}'.format(message))
+            message = "Opened honeypot for {}, resulting in {} credits.{}".format(user.me()['email'], result["credits"], user_balance)
         else:
             message = "Couldn\'t open honeypot for {}, unknown error.{}".format(user.me()['email'], user_balance)
-            webhook = DiscordWebhook(url=DISCORD, content='{}'.format(message))
     else:
         message = "Couldn\'t open honeypot for {}, did not gather 15MB.{}\nProgress in bytes: {}/15000000".format(user.me()['email'], user_balance, user.get_honeypot_status()["progress_bytes"])
-        webhook = DiscordWebhook(url=DISCORD, content='{}'.format(message))
 
 print(message)
+webhook = DiscordWebhook(url=DISCORD, content='{}'.format(message))
 response = webhook.execute()
